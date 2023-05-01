28f20c9070d16fe798a0a202fed0141458659a53
soprue <soprue@naver.com>
2023-05-02 00:31:00 +0900

    Feat : 하단 메뉴 구현

--- pages/Home.js
+++ pages/Home.js
@@ -1,16 +1,70 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 function Home({ navigation }) {
     return (
-        <View>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Write"
-                onPress={() => navigation.navigate("Write")}
-            />
+        <View style={styles.home}>
+            <View style={styles.menu}>
+                <TouchableOpacity
+                    activeOpacity="0.6"
+                    style={{
+                        ...styles.menuTab,
+                        borderTopLeftRadius: 10,
+                        borderBottomLeftRadius: 10,
+                        borderRightWidth: 1,
+                        borderRightColor: "#eee",
+                    }}
+                >
+                    <View>
+                        <Feather name="file-plus" size={24} color="black" />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    activeOpacity="0.6"
+                    style={{
+                        ...styles.menuTab,
+                        borderTopRightRadius: 10,
+                        borderBottomRightRadius: 10,
+                    }}
+                    onPress={() => navigation.navigate("작성하기")}
+                >
+                    <View>
+                        <Feather name="folder-plus" size={24} color="black" />
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    home: {
+        width: "100%",
+        height: "100%",
+    },
+    menu: {
+        position: "absolute",
+        left: "0%",
+        bottom: 10,
+        flexDirection: "row",
+        // borderRadius: 10,
+        shadowColor: "#000",
+        shadowOpacity: 0.06,
+        shadowOffset: {
+            width: 10,
+            height: 10,
+        },
+        paddingHorizontal: 20,
+    },
+    menuTab: {
+        backgroundColor: "#fff",
+        flex: 1,
+        alignItems: "center",
+        paddingVertical: 20,
+    },
+});
 
 export default Home;

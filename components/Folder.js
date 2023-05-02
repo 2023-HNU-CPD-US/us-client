e08da1d0b25718d23e171452cdf6413bd6ef2870
soprue <soprue@naver.com>
2023-05-02 23:48:47 +0900

    Feat : 폴더 컴포넌트 생성

---
+++ components/Folder.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+import { Entypo } from "@expo/vector-icons";
+
+function Folder({ name }) {
+    return (
+        <TouchableOpacity activeOpacity="0.6" style={styles.folder}>
+            <TouchableOpacity activeOpacity="0.6" style={styles.folderMenu}>
+                <Entypo name="dots-three-vertical" size={20} color="#777" />
+            </TouchableOpacity>
+            <View>
+                <Image
+                    style={{ width: "60%", height: undefined, aspectRatio: 1 }}
+                    resizeMode="contain"
+                    source={require("../assets/folder.png")}
+                ></Image>
+                <Text style={styles.folderName}>{name}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    folder: {
+        width: "48%",
+        backgroundColor: "#fff",
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+    },
+    folderMenu: {
+        position: "absolute",
+        top: 20,
+        right: 10,
+    },
+    folderName: {
+        fontSize: 16,
+        fontWeight: 600,
+        marginTop: 5,
+    },
+});
+
+export default Folder;
--- pages/Home.js
+++ pages/Home.js
@@ -3,10 +3,23 @@ import { StyleSheet, View, Text } from "react-native";
 
 import { Feather } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
+import Folder from "../components/Folder";
+import { ScrollView } from "react-native";
 
 function Home({ navigation }) {
     return (
         <View style={styles.home}>
+            <ScrollView style={styles.list}>
+                <View style={styles.listRow}>
+                    <Folder name="Food Recipes"></Folder>
+                    <Folder name="Project"></Folder>
+                </View>
+                <View style={styles.listRow}>
+                    <Folder name="Secret"></Folder>
+                    <Folder name="Reference"></Folder>
+                </View>
+            </ScrollView>
+
             <View style={styles.menu}>
                 <TouchableOpacity
                     activeOpacity="0.6"
@@ -17,6 +30,7 @@ function Home({ navigation }) {
                         borderRightWidth: 1,
                         borderRightColor: "#eee",
                     }}
+                    onPress={() => navigation.navigate("작성하기")}
                 >
                     <View>
                         <Feather name="file-plus" size={24} color="black" />
@@ -29,7 +43,7 @@ function Home({ navigation }) {
                         borderTopRightRadius: 10,
                         borderBottomRightRadius: 10,
                     }}
-                    onPress={() => navigation.navigate("작성하기")}
+                    // onPress={() => navigation.navigate("작성하기")}
                 >
                     <View>
                         <Feather name="folder-plus" size={24} color="black" />
@@ -44,6 +58,15 @@ const styles = StyleSheet.create({
     home: {
         width: "100%",
         height: "100%",
+    },
+    list: {
+        width: "100%",
+        paddingHorizontal: 20,
+    },
+    listRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 20,
     },
     menu: {
         position: "absolute",
